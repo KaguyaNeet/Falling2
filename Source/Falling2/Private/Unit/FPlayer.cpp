85d205c8aa58d1a49d4366d163403d4c87e922bd
DESKTOP-KDORPQV\p5524 <552448191@qq.com>
2018-09-28 20:53:52 +0800

    Base files

---
+++ Source/Falling2/Private/Unit/FPlayer.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPlayer.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Classes/Components/InputComponent.h"
+#include "Classes/Components/SkeletalMeshComponent.h"
+#include "Classes/Camera/CameraComponent.h"
+#include "Classes/GameFramework/SpringArmComponent.h"
+#include "Classes/Components/ArrowComponent.h"
+
+
+
+AFPlayer::AFPlayer()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->AttachTo(RootComponent);
+	SpringArm->TargetArmLength = 700.f;
+	SpringArm->SetRelativeRotation(FRotator(0.f, -40.f, 0.f));
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bUsePawnControlRotation = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->AttachTo(SpringArm);
+
+	CorrectionArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("CorrectionArrow"));
+	CorrectionArrow->AttachTo(RootComponent);
+	CorrectionArrow->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+	GetMesh()->AttachTo(CorrectionArrow);
+}
+
+void AFPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* controller = UGameplayStatics::GetPlayerController(this, 0))
+	{
+		controller->bShowMouseCursor = true;
+	}
+}
+
+void AFPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (APlayerController* controller = UGameplayStatics::GetPlayerController(this, 0))
+	{
+		FHitResult hit;
+		controller->GetHitResultUnderCursorByChannel(ETraceTypeQuery::TraceTypeQuery1, true, hit) ? lastHit = hit : hit = lastHit;
+		FRotator rotation = GetMesh()->GetRelativeTransform().Rotator();
+		FVector direction = hit.Location - GetActorLocation();
+		FVector directionXY = FVector(direction.X, direction.Y, 0.f);
+		
+		float offset = (directionXY.Rotation().Yaw - rotation.Yaw) / 180.f;
+		if (FMath::Abs(offset) > 1.f)
+		{
+			float sign;
+			sign = rotation.Yaw >= 0.f ? 1.f : -1.f;
+			offset = sign * (360.f - (FMath::Abs(directionXY.Rotation().Yaw) + FMath::Abs(rotation.Yaw))) / 180.f;
+			RightTurn = sign > 0.f ? true : false;
+		}
+		if (!Turn)
+		{
+			if (FMath::Abs(offset) > 0.48f)
+				Turn = true;
+		}
+		else
+		{
+			GetMesh()->SetRelativeRotation(UKismetMathLibrary::RLerp(rotation, directionXY.Rotation(), DeltaTime * 5.f, true));
+			if (FMath::Abs(offset) <= 0.05f)
+				Turn = false;
+		}
+		CurrentOffset = UKismetMathLibrary::Lerp(CurrentOffset, offset, DeltaTime * 5.f);
+
+		FVector velocity = GetVelocity();
+		if (velocity.Size2D() > 0.f)
+		{
+			float sign;
+			velocity.Rotation().Yaw >= 0.f ? sign = 1.f : sign = -1.f;
+			float velocityOffset = directionXY.Rotation().Yaw - velocity.Rotation().Yaw;
+			VelocityOffset = FMath::Abs(velocityOffset) > 180.f ? (360.f - FMath::Abs(velocityOffset)) * sign : velocityOffset;
+		}
+		else
+		{
+			VelocityOffset = 0.f;
+		}
+	}
+}
+
+
+void AFPlayer::SetupPlayerInputComponent(class UInputComponent* inputComponent)
+{
+	Super::SetupPlayerInputComponent(inputComponent);
+
+	inputComponent->BindAxis("MoveForward", this, &AFPlayer::MoveForward);
+	inputComponent->BindAxis("MoveRight", this, &AFPlayer::MoveRight);
+}
+
+void AFPlayer::MoveForward(float value)
+{
+	AddMovementInput(FVector(1.f, 0.f, 0.f), value);
+}
+
+void AFPlayer::MoveRight(float value)
+{
+	AddMovementInput(FVector(0.f, 1.f, 0.f), value);
+}
---
+++ Source/Falling2/Public/Unit/FCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Unit/FBaseUnit.h"
+#include "FCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FALLING2_API AFCharacter : public AFBaseUnit
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
