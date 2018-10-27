ba112cab11121e2554ee72871a5f55551a5cfac0
DESKTOP-KDORPQV\p5524 <552448191@qq.com>
2018-10-27 22:43:42 +0800

    Room system

---
+++ Source/Falling2/Private/Room/FMap.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FMap.h"
+
+
+// Sets default values
+AFMap::AFMap()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFMap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFMap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
