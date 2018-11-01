7755081816e528af9e3de243f1cb93892fdaf6f6
DESKTOP-KDORPQV\p5524 <552448191@qq.com>
2018-11-01 22:23:35 +0800

    Room files

---
+++ Source/Falling2/Private/Room/FRoomPortal.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FRoomPortal.h"
+
+
+// Sets default values
+AFRoomPortal::AFRoomPortal()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFRoomPortal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFRoomPortal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
