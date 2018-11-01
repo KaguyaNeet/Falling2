5371f8e1756fda1b3a95f218ab73e9f180795600
DESKTOP-KDORPQV\p5524 <552448191@qq.com>
2018-11-01 22:23:24 +0800

    Player components

--- Source/Falling2/Private/Unit/FCharacterPlayer.cpp
+++ Source/Falling2/Private/Unit/FCharacterPlayer.cpp
@@ -2,6 +2,45 @@
 
 #include "FCharacterPlayer.h"
 
+#include "Classes/Components/InputComponent.h"
+#include "Classes/Components/SkeletalMeshComponent.h"
+#include "Classes/Camera/CameraComponent.h"
+#include "Classes/GameFramework/SpringArmComponent.h"
+#include "Classes/Components/ArrowComponent.h"
+#include "Classes/Components/CapsuleComponent.h"
+
+
+AFCharacterPlayer::AFCharacterPlayer()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->AttachTo(RootComponent);
+	SpringArm->TargetArmLength = 700.f;
+	SpringArm->SetRelativeRotation(FRotator(0.f, -40.f, 0.f));
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bUsePawnControlRotation = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->AttachTo(SpringArm);
+
+	CorrectionArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("CorrectionArrow"));
+	CorrectionArrow->AttachTo(RootComponent);
+	CorrectionArrow->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+
+	GetMesh()->AttachTo(CorrectionArrow);
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	GetMesh()->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	GetMesh()->SetCollisionObjectType(ECollisionChannel::ECC_GameTraceChannel3);
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel2, ECollisionResponse::ECR_Block);
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel4, ECollisionResponse::ECR_Block);
+
+
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	GetCapsuleComponent()->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel1, ECollisionResponse::ECR_Overlap);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel4, ECollisionResponse::ECR_Ignore);
+}
 
 
 void AFCharacterPlayer::SetupPlayerInputComponent(UInputComponent* inputComponent)
@@ -17,6 +56,19 @@ void AFCharacterPlayer::SetupPlayerInputComponent(UInputComponent* inputComponen
 	inputComponent->BindAction("SecondaryFire", IE_Released, this, &AFCharacterPlayer::RightMouseButtonUp);
 	inputComponent->BindAction("Rolling", IE_Pressed, this, &AFCharacterPlayer::TriggerRolling);
 	inputComponent->BindAction("Pick", IE_Pressed, this, &AFCharacterPlayer::PickUp);
+}
+
+void AFCharacterPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+}
+
+void AFCharacterPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
 }
 
 void AFCharacterPlayer::MoveForward(float value)
--- Source/Falling2/Public/Unit/FCharacterPlayer.h
+++ Source/Falling2/Public/Unit/FCharacterPlayer.h
@@ -14,10 +14,30 @@ class FALLING2_API AFCharacterPlayer : public AFCharacterUnit
 {
 	GENERATED_BODY()
 	
+
 public:
+	//Cameraboom of this player.
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+		class USpringArmComponent* SpringArm;
+	//Main camera of this game.
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+		class UCameraComponent* Camera;
+	//The arrow help for repairing the mesh direction.
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+		class UArrowComponent* CorrectionArrow;
+
+public:
+	// Constructor
+	AFCharacterPlayer();
+
 	// Input function
 	virtual void SetupPlayerInputComponent(class UInputComponent* inputComponent) override;
-	
+
+	// Begin play function
+	virtual void BeginPlay() override;
+
+	// Tick function
+	virtual void Tick(float DeltaTime) override;
 private:
 
 	// These functions are attaching with the input component
