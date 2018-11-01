148f74826ab647668fe4bccfc18271f557610711
DESKTOP-KDORPQV\p5524 <552448191@qq.com>
2018-11-01 21:38:54 +0800

    Add input event

--- Source/Falling2/Private/Unit/FCharacterPlayer.cpp
+++ Source/Falling2/Private/Unit/FCharacterPlayer.cpp
@@ -4,4 +4,49 @@
 
 
 
+void AFCharacterPlayer::SetupPlayerInputComponent(UInputComponent* inputComponent)
+{
+	Super::SetupPlayerInputComponent(inputComponent);
 
+	inputComponent->BindAxis("MoveForward", this, &AFCharacterPlayer::MoveForward);
+	inputComponent->BindAxis("MoveRight", this, &AFCharacterPlayer::MoveRight);
+
+	inputComponent->BindAction("MainFire", IE_Pressed, this, &AFCharacterPlayer::LeftMouseButtonDown);
+	inputComponent->BindAction("MainFire", IE_Released, this, &AFCharacterPlayer::LeftMouseButtonUp);
+	inputComponent->BindAction("SecondaryFire", IE_Pressed, this, &AFCharacterPlayer::RightMouseButtonDown);
+	inputComponent->BindAction("SecondaryFire", IE_Released, this, &AFCharacterPlayer::RightMouseButtonUp);
+	inputComponent->BindAction("Rolling", IE_Pressed, this, &AFCharacterPlayer::TriggerRolling);
+	inputComponent->BindAction("Pick", IE_Pressed, this, &AFCharacterPlayer::PickUp);
+}
+
+void AFCharacterPlayer::MoveForward(float value)
+{
+}
+
+void AFCharacterPlayer::MoveRight(float value)
+{
+}
+
+void AFCharacterPlayer::LeftMouseButtonDown()
+{
+}
+
+void AFCharacterPlayer::LeftMouseButtonUp()
+{
+}
+
+void AFCharacterPlayer::RightMouseButtonDown()
+{
+}
+
+void AFCharacterPlayer::RightMouseButtonUp()
+{
+}
+
+void AFCharacterPlayer::TriggerRolling()
+{
+}
+
+void AFCharacterPlayer::PickUp()
+{
+}
--- Source/Falling2/Public/Unit/FCharacterPlayer.h
+++ Source/Falling2/Public/Unit/FCharacterPlayer.h
@@ -14,7 +14,22 @@ class FALLING2_API AFCharacterPlayer : public AFCharacterUnit
 {
 	GENERATED_BODY()
 	
+public:
+	// Input function
+	virtual void SetupPlayerInputComponent(class UInputComponent* inputComponent) override;
 	
-	
-	
+private:
+
+	// These functions are attaching with the input component
+	void MoveForward(float value);
+	void MoveRight(float value);
+
+	void LeftMouseButtonDown();
+	void LeftMouseButtonUp();
+	void RightMouseButtonDown();
+	void RightMouseButtonUp();
+
+	void TriggerRolling();
+
+	void PickUp();
 };
